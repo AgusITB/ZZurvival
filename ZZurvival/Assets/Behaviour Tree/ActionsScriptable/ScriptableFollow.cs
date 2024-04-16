f37a5b4ded585927ec3615f3e6fafa82b8c35f90
AgusITB <agustin.garibotto.7e6@itb.cat>
2024-04-16 20:48:06 +0200

    navmesh test

--- ZZurvival/Assets/Behaviour Tree/ActionsScriptable/ScriptableFollow.cs
+++ ZZurvival/Assets/Behaviour Tree/ActionsScriptable/ScriptableFollow.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu(fileName = "ScriptableFollow", menuName = "ScriptableObjects2/ScriptableAction/ScriptableFollow")]
 
