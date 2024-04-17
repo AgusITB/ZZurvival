daa5f5202453b054c0fcfaebdda056ceb7ca8b3b
AgusITB <agustin.garibotto.7e6@itb.cat>
2024-04-17 19:29:39 +0200

    playerdetection

--- ZZurvival/Assets/Behaviour Tree/EnemyController3.cs
+++ ZZurvival/Assets/Behaviour Tree/EnemyController3.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemyController3 : StateController2
@@ -5,6 +6,7 @@ public class EnemyController3 : StateController2
     public float AttackDistance;
     public float HP;
     private float nextHurt = 0;
+    private float detection_delay;
 
     void Update()
     {
@@ -28,16 +30,78 @@ public class EnemyController3 : StateController2
     {
         if (collision.TryGetComponent(out PlayerMovement player))
         {
-            Debug.Log("I saw the player");
-            target = player.gameObject;
+            Vector3 direction = player.transform.position - transform.position;
+            Debug.DrawRay(transform.position, direction * 500f, Color.green);
+
+            LayerMask layermask = LayerMask.NameToLayer("Wall");
+
+            RaycastHit[] hits = Physics.RaycastAll(transform.position, direction, 500f);
+            foreach (RaycastHit item in hits)
+            {
+                Debug.Log(item.transform.name);
+            }
+            for (int i = 0; i < hits.Length; i++)
+            {
+                RaycastHit hit = hits[i];
+    
+                if (hit.collider.gameObject.layer == layermask)
+                {
+                    Debug.Log("Can't see the player");
+                    break;
+                }else
+                {
+                    Debug.Log("I saw the player");
+                    target = player.gameObject;
+                }
+        
+            }
+
         }
+    }
+    private Vector3[] GetBoundingPoints(Bounds bounds)
+    {
+        Vector3[] bounding_points =
+        {
+        bounds.min,
+        bounds.max,
+        new Vector3( bounds.min.x, bounds.min.y, bounds.max.z ),
+        new Vector3( bounds.min.x, bounds.max.y, bounds.min.z ),
+        new Vector3( bounds.max.x, bounds.min.y, bounds.min.z ),
+        new Vector3( bounds.min.x, bounds.max.y, bounds.max.z ),
+        new Vector3( bounds.max.x, bounds.min.y, bounds.max.z ),
+        new Vector3( bounds.max.x, bounds.max.y, bounds.min.z )
+    };
+
+        return bounding_points;
+    }
+
+    private IEnumerator DetectPlayer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(detection_delay);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        
     }
     private void OnTriggerExit(Collider collision)
     {
-        if (collision.TryGetComponent(out PlayerMovement _))
-        {
+        if (collision.TryGetComponent(out PlayerMovement player))
+        {         
             target = null;
         }
         
     }
+    private void OnDrawGizmos()
+    {
+        if(target!= null)
+        {
+            Vector3 direction = target.transform.position - transform.position;
+            Debug.DrawRay(transform.position, direction * 500f, Color.green);
+        }
+      
+    }
+
 }
